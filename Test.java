93c5c5b78aab910134ac1f1a187b972285e9c330
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2024-10-06 02:21:41 +0500

    Delete the test file and switch to main class

--- Test.java
+++
@@ -1,7 +0,0 @@
-
-public class Test {
-    public static void main(String[] args) {
-        Screening screening = new Screening("Iron man 2", "07:30 PM");
-        System.out.println(screening.getScreeningDetails());
-    }
-}
